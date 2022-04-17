1b1e2742169b6e394f8d45e0fc80beb069f48f72
iinaamasum <iinaamasum@gmail.com>
2022-04-17 16:58:31 +0600

    404 not found done

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import error from '../../images/404-error-page-not-found.jpg';
 
 const Error404 = () => {
-  return <div>404 not found</div>;
+  return (
+    <div className="">
+      <img className="w-full h-[70vh]" src={error} alt="" />
+    </div>
+  );
 };
 
 export default Error404;
