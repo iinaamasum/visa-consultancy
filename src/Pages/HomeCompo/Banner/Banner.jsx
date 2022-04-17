2c337ccd621a410f9a9332c350d6e87ab6c1aeab
iinaamasum <iinaamasum@gmail.com>
2022-04-17 06:03:45 +0600

    banner customization

--- src/Pages/HomeCompo/Banner/Banner.jsx
+++ src/Pages/HomeCompo/Banner/Banner.jsx
@@ -6,9 +6,19 @@ const Banner = () => {
     <div className="w-full">
       <div
         style={{ backgroundImage: `url(${banner1})` }}
-        className="h-[50vh] bg-no-repeat bg-cover"
+        className="h-[50vh] bg-no-repeat bg-cover flex items-center justify-center"
       >
-        hi there
+        <div className="h-full w-full flex items-center justify-center backdrop-blur-sm">
+          <div className="text-center">
+            <h1 className="text-4xl md:text-6xl tracking-normal text-black font-bold mb-3">
+              Welcome to Visa Consultancy and Tourism Service
+            </h1>
+            <h3 className="text-2xl text-slate-700 ">
+              I am here to provide you proper guidance to achieve your desired
+              visa. Please visit our services section.
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="flex flex-wrap items-center justify-between px-2 py-3 bg-white pb-3 shadow sticky top-0">
+      <nav className="flex flex-wrap items-center justify-between px-2 py-3 bg-white pb-3 shadow sticky top-0 z-50">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
