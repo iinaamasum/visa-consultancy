2719f4a0a540d2a4d9b6f0b9cedf0668b0e771db
iinaamasum <iinaamasum@gmail.com>
2022-04-18 20:36:05 +0600

    checkout page content done

--- src/Pages/HomeCompo/Reviews/Reviews.jsx
+++ src/Pages/HomeCompo/Reviews/Reviews.jsx
@@ -41,7 +41,7 @@ const Reviews = () => {
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              My Honorable Client Reviews
+              My Honorable Clients Reviews
             </h1>
             <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           </div>
